bb58ed5f4cf0b0e0c81725acce4c3a096bd34b89
joyc <joyc@live.jp>
2019-07-22 00:26:11 +0900

    use flask_migrate

---
+++ restdemo/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from restdemo.resource.user import User, UserList
+from restdemo.resource.hello import HelloWorld
+
+
+db = SQLAlchemy()
+
+def create_app():
+
+    app = Flask(__name__)
+    api = Api(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///demo.db"
+    db = SQLAlchemy()
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    api.add_resource(HelloWorld, '/')
+    api.add_resource(User, '/user/<string:username>')
+    api.add_resource(UserList, '/users')
+
+    return app
