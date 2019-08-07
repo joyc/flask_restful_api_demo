110f60701072508940213eae117a458e2cdf3686
joyc <joyc@live.jp>
2019-08-07 13:14:13 +0900

    add test config

--- restdemo/config.py
+++ restdemo/config.py
@@ -3,11 +3,33 @@ import os
 
 
 class Config:
-    #SQLALCHEMY_DATABASE_URI = "sqlite:///demo.db"
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@10.211.55.4:3306/api_demo'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'flask123')
     JWT_AUTH_URL_RULE = '/auth/login'
     JWT_AUTH_ENDPOINT = 'JWT'
     JWT_EXPIRATION_DELTA = timedelta(seconds=300)
 
+
+class TestingConfig(Config):
+    """Config for testing"""
+    JWT_AUTH_ENDPOINT = 'FLASK'
+    SECRET_KEY = "flask123"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+
+
+class DevelopmentConfig(Config):
+    """Config for Development"""
+    JWT_AUTH_ENDPOINT = 'FLASK'
+    SECRET_KEY = "flask123"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@10.211.55.4:3306/api_demo"
+
+
+class ProductionConfig(Config):
+    ...
+
+
+app_config = {
+    'testing': TestingConfig,
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
---
+++ restdemo/model/base.py
@@ -0,0 +1,21 @@
+from restdemo import db
+
+
+class Base(db.Model):
+
+    __abstract__ = True
+
+    def as_dict(self):
+        """使序列化返回字典"""
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+    def add(self):
+        db.session.add(self)
+        db.session.commit()    
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
