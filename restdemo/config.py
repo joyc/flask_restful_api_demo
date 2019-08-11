7f9165e169913a0d8569ed9ada52b16b411bbff9
joyc <joyc@live.jp>
2019-08-11 22:11:22 +0900

    change unittest config file

--- restdemo/config.py
+++ restdemo/config.py
@@ -6,20 +6,21 @@ class Config:
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_AUTH_URL_RULE = '/auth/login'
-    JWT_AUTH_HEADER_ENDPOINT = 'FLASK'
+    JWT_AUTH_HEADER_PREFIX = os.environ.get('JWT_AUTH_HEADER_PREFIX', 'FLASK')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'flask123')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     JWT_EXPIRATION_DELTA = timedelta(seconds=300)
 
 
 class TestingConfig(Config):
     """Config for testing"""
-    SECRET_KEY = "flask123"
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
 
 
 class DevelopmentConfig(Config):
     """Config for Development"""
-    SECRET_KEY = "flask123"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@10.211.55.4:3306/api_demo"
+    DEBUG = True
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@10.211.55.4:3306/api_demo'
 
 
 class ProductionConfig(Config):
